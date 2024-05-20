266b7269893927e4e8f6ed9fe357b1f06698f7a2
Bharathkumar V <154517745+Bharath4624@users.noreply.github.com>
2024-05-20 20:53:58 +0530

    Time: 0 ms (100.00%), Space: 44.4 MB (41.09%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.java
@@ -14,26 +14,58 @@
  * }
  */
 class Solution {
-    public int kthSmallest(TreeNode root, int k) {
-        if(root==null){
-            return root.val;
+    // public int kthSmallest(TreeNode root, int k) {
+    //     if(root==null){
+    //         return root.val;
+    //     }
+    //     int[] small=new int[1];
+    //     int count[]=new int[1];
+    //     count[0]=0;
+    //     inorder(root,k,small,count);
+    //     return small[0];
+    // }
+    // public void inorder(TreeNode root,int k,int[] small,int[] count){
+    //     if(root==null){
+    //         return;
+    //     }
+    //     inorder(root.left,k,small,count);
+    //     count[0]++;
+    //     if(count[0]==k){
+    //         small[0]=root.val;
+    //         return;
+    //     }
+    //     inorder(root.right,k,small,count);
+    // }
+    public int kthSmallest(TreeNode root, int k){
+        TreeNode curr=root;
+        int count=0;
+        while(curr!=null){
+            if(curr.left==null){
+                count++;
+                if(count==k){
+                    return curr.val;
+                }
+                curr=curr.right;
+            }
+            else{
+                TreeNode prev=curr.left;
+                while(prev.right!=null && prev.right!=curr){
+                    prev=prev.right;
+                }
+                if(prev.right==null){
+                    prev.right=curr;
+                    curr=curr.left;
+                }
+                else{
+                    prev.right=null;
+                    count++;
+                    if(count==k){
+                        return curr.val;
+                    }
+                    curr=curr.right;
+                }
+            }
         }
-        int[] small=new int[1];
-        int count[]=new int[1];
-        count[0]=0;
-        inorder(root,k,small,count);
-        return small[0];
-    }
-    public void inorder(TreeNode root,int k,int[] small,int[] count){
-        if(root==null){
-            return;
-        }
-        inorder(root.left,k,small,count);
-        count[0]++;
-        if(count[0]==k){
-            small[0]=root.val;
-            return;
-        }
-        inorder(root.right,k,small,count);
+        return -1;
     }
 }
