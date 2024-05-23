ead3776d977b4e34d86f22c546efa7fa5c63d6cb
Bharathkumar V <154517745+Bharath4624@users.noreply.github.com>
2024-05-23 19:33:39 +0530

    Time: 3 ms (85.21%), Space: 49.4 MB (55.17%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,33 +1,34 @@
 class Solution {
-    int rows;
-    int cols;
-    int islands = 0;
     public int numIslands(char[][] grid) {
-        rows = grid.length;
-        cols = grid[0].length;
-        for (int row = 0; row < rows; row++)
-            check(grid, row);
-        return islands;
-    }
-
-    public void check(final char[][] grid, int row) {
-        final char[] finalRow = grid[row];
-        for (int col = 0; col < cols; ++col)
-            if (finalRow[col] == '1') {
-                bfs(grid, row, col);
-                ++islands;
+        int n=grid.length;
+        int m=grid[0].length;
+        int[][] vis=new int[n][m];
+        int count=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]=='1' && vis[i][j]==0){
+                    dfs(i,j,grid,vis);
+                    count++;
+                }
             }
+        }
+        return count;
     }
-
-    public void bfs(char[][] grid, int row, int col) {
-        grid[row][col] = '*';
-        if (row > 0 && grid[row - 1][col] == '1')
-            bfs(grid, row - 1, col);
-        if (row + 1 < rows && grid[row + 1][col] == '1')
-            bfs(grid, row + 1, col);
-        if (col > 0 && grid[row][col - 1] == '1')
-            bfs(grid, row, col - 1);
-        if (col + 1 < cols && grid[row][col + 1] == '1')
-            bfs(grid, row, col + 1);
+    public void dfs(int row,int col,char[][] grid,int[][] vis){
+        int n=grid.length;
+        int m=grid[0].length;
+        vis[row][col]=1;
+        if(col+1<m && vis[row][col+1]==0 && grid[row][col+1]=='1'){
+            dfs(row,col+1,grid,vis);
+        }
+        if(col-1>=0 && vis[row][col-1]==0 && grid[row][col-1]=='1'){
+            dfs(row,col-1,grid,vis);
+        }
+        if(row-1>=0 && vis[row-1][col]==0 && grid[row-1][col]=='1'){
+            dfs(row-1,col,grid,vis);
+        }
+        if(row+1<n && vis[row+1][col]==0 && grid[row+1][col]=='1'){
+            dfs(row+1,col,grid,vis);
+        }
     }
 }
