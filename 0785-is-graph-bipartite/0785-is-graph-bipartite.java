45831f4c251035b964383919aa058728317a66aa
Bharathkumar V <154517745+Bharath4624@users.noreply.github.com>
2024-05-25 18:44:15 +0530

    Time: 0 ms (100.00%), Space: 44.4 MB (95.74%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -6,28 +6,41 @@ class Solution {
         Arrays.fill(color, -1);
         for (int i = 0; i < v; i++) {
             if (color[i] == -1) {
-                if (!bfsCheck(graph, color, i)) {
+                if (!dfsCheck(graph,0, color, i)) {
                     return false;
                 }
             }
         }
         return true;
     }
-    public boolean bfsCheck(int[][] graph, int[] color, int start) {
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(start);
-        color[start] = 0;
-        while (!queue.isEmpty()) {
-            int node = queue.poll();
-            for (int neighbor : graph[node]) {
+    // public boolean bfsCheck(int[][] graph, int[] color, int start) {
+    //     Queue<Integer> queue = new LinkedList<>();
+    //     queue.add(start);
+    //     color[start] = 0;
+    //     while (!queue.isEmpty()) {
+    //         int node = queue.poll();
+    //         for (int neighbor : graph[node]) {
+    //             if (color[neighbor] == -1) {
+    //                 color[neighbor] = 1 - color[node];
+    //                 queue.add(neighbor);
+    //             } else if (color[neighbor] == color[node]) {
+    //                 return false;
+    //             }
+    //         }
+    //     }
+    //     return true;
+    // }
+     public boolean dfsCheck(int[][] graph,int col, int[] color, int start) {
+        color[start] = col;
+        for (int neighbor : graph[start]) {
                 if (color[neighbor] == -1) {
-                    color[neighbor] = 1 - color[node];
-                    queue.add(neighbor);
-                } else if (color[neighbor] == color[node]) {
+                    if(!dfsCheck(graph,1-col,color,neighbor)){
+                        return false;
+                    }
+                } else if (color[neighbor] == col) {
                     return false;
                 }
-            }
         }
         return true;
-    }
+     }
 }
