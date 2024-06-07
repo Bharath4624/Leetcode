4a0109276c661aee6344716f5f3274636e0b01a3
Bharathkumar V <154517745+Bharath4624@users.noreply.github.com>
2024-06-07 15:25:41 +0530

    Time: 5 ms (41.31%), Space: 45.9 MB (12.43%) - LeetHub

---
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String reverseVowels(String s) {
+        StringBuilder sb=new StringBuilder(s);
+        int i=0;
+        int j=s.length()-1;
+        while(i<=j){
+            if((s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u' || s.charAt(i)=='A' || s.charAt(i)=='E' || s.charAt(i)=='I' || s.charAt(i)=='O' || s.charAt(i)=='U') && (s.charAt(j)=='a' || s.charAt(j)=='e' || s.charAt(j)=='i' || s.charAt(j)=='o' || s.charAt(j)=='u' || s.charAt(j)=='A' || s.charAt(j)=='E' || s.charAt(j)=='I' || s.charAt(j)=='O' || s.charAt(j)=='U')){
+                char ch=s.charAt(i);
+                sb.setCharAt(i,s.charAt(j));
+                sb.setCharAt(j,ch);
+                i++;
+                j--;
+            }
+            else if(s.charAt(i)=='a' || s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u' || s.charAt(i)=='A' || s.charAt(i)=='E' || s.charAt(i)=='I' || s.charAt(i)=='O' || s.charAt(i)=='U'){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+        return sb.toString();
+    }
+}
